9589269e62dea1a75eda92d6a2e9cca5bf8125c8
Jason Dryhurst-Smith <jason@jasonds.co.uk>
2017-09-19 19:58:51 +0100

    add some code examples for the first three topics

---
+++ FunctionalIDlondon/src/IDinLondon/IDinLondon/CentralExceptionHandling.fs.fs
@@ -0,0 +1,35 @@
+﻿namespace IDinLondon
+
+module Ceh = 
+
+    open IDinLondon.Dt
+
+    // A cause of failure 
+    type Failure = 
+        | NotImplemented
+
+    // A nice ... ahem ... monadic type to hold everything
+    type Result<'a> =
+        | Success of 'a
+        | Failure of Failure
+
+    // Our function to do the work
+    let updateTheThing request value = 
+        if value > 5 then
+            Failure NotImplemented
+        else
+            Success (transform request value)
+
+    // Our 'controller' function
+    let doWork request = 
+        let dbValue = { Key = "userName"; Value = 5 }    
+        let response = updateTheThing dbValue request  
+        
+        match response with
+        | Success response -> response.ToString()
+        | Failure fail ->
+            match fail with
+            | NotImplemented -> "Not implemented!"
+
+    
+
---
+++ FunctionalIDlondon/src/IDinLondon/IDinLondon/Infrastructure.fs
@@ -0,0 +1,39 @@
+﻿namespace IDinLondon
+
+module Inf = 
+
+    open System.Configuration
+
+    open IDinLondon.Dt
+
+    // Some bit of infrastructure we need all the time
+    type DatabaseConnection(connectionStringName : string) = 
+        let connection = ConfigurationManager.ConnectionStrings.Item(connectionStringName)
+
+        member x.save value = 
+            value
+                
+    // Build some infrastructure
+    let saveValueInTheDatabase (connection : DatabaseConnection) value =
+        connection.save(value)
+            
+    let saveValueInMyDatabase value = 
+        saveValueInTheDatabase <| DatabaseConnection "MyDatabase"
+        
+
+    // No more DI hell! Just type signatures
+    let doWork value = 
+        transform { Key = "userName"; Value = 3 } value
+        |> saveValueInMyDatabase
+
+
+    // You can even make this stuff easy to test
+    let doWorkSuperDuperTestable value saveFn =
+        transform { Key = "userName"; Value = 3 } value
+        |> saveFn
+
+    let test =  
+        let expected = { Key = "userName"; Value = 4 }
+        let result = doWorkSuperDuperTestable 4 (fun value -> value)
+        expected = result
+    
